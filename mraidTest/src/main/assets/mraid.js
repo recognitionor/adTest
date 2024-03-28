35799274a48e5cc983d879d9ea11ee305fd2e72d
junghee <junghee@nhn.com>
2024-03-28 11:45:48 +0900

    mraid test

--- mraidTest/src/main/assets/mraid.js
+++ mraidTest/src/main/assets/mraid.js
@@ -1,19 +1,90 @@
 var MRAID_ENV = {};
-var mraid = {
+var listeners = [];
+listeners['stateChange'] = [];
+var state = 'loading'; //Can be loading, default, expanded, hidden, or resized
 
-    testPrint: function() {
+var mraid = {
+    nativeCall: function (url) {
+        window.location = url
+    },
+
+    open: function () {
+
+    },
+
+    fireEvent: function (event) {
+        // this.nativeCall("mraid://test");
+        document.location = "mraid://showToast";
+        // Device.showToast("32");
+        console.log("fireEvent : " + event);
+        if (!listeners[event]) {
+            console.log("1");
+            return;
+        }
+        console.log("2");
+        var args = Array.prototype.slice.call(arguments);
+        args.shift();
+        var length = listeners[event].length;
+        console.log("length : " + length);
+        for (var i = 0; i < length; i++) {
+            console.log("for : " + i);
+            if (typeof listeners[event][i] === "function") {
+                console.log("3");
+                listeners[event][i].apply(null, args);
+            }
+        }
+    },
+
+    testPrint: function () {
         console.log("텔미텔미 테테테테레레레텔미~~");
         return "텔미텔미 테테테테레레레텔미"
     },
 
-    open: function(url) {
-        console.log("open  : " + url);
+    open: function (url) {
+        // this.util.fireEvent("test")
+        console.log("open  : " + MRAID_ENV.version);
+        Device.open(url);
+        console.log("open  : " + MRAID_ENV.version);
     },
 
-    getVersion: function() {
-        return "window.MRAID_ENV.version";
+    getVersion: function () {
+        return MRAID_ENV.version;
     },
 
-    getState: function() {
+    getState: function () {
+    },
+
+    addEventListener: function (event_name, method) {
+        if (listeners) {
+            console.log("1 : " + listeners)
+        } else {
+            console.log("2 : " + listeners)
+        }
+        console.log("addEventListener : " + listeners)
+        console.log("addEventListener : " + listeners.length)
+        if (!listeners) return; // Listener is already registered
+        console.log("event_name : " + event_name)
+        if (event_name == 'stateChange') {
+            console.log("stateChange!!!")
+        }
+        listeners[event_name].push(method);
+    },
+
+    removeEventListener: function (event_name, method) {
+        //If no method name is given, remove all listeners from event
+        if (method == null) {
+            listeners[event_name].length = 0;
+            return;
+        }
+
+        var method_index = listeners[event_name].indexOf(method);
+        if (method_index > -1) { //Don't try to remove unregistered listeners
+            listeners[event_name].splice(method_index, 1);
+        } else {
+            mraid.util.errorEvent("An unregistered listener was requested to be removed.", "mraid.removeEventListener()")
+        }
+    }, onReadyEvent: function () {
+        console.log("onReady")
     }
+
 };
--- mraidTest/src/main/java/com/jhlee/mraidtest/MainActivity.kt
+++ mraidTest/src/main/java/com/jhlee/mraidtest/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.jhlee.mraidtest
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         // MRAID 스크립트 로드
         mWebView.loadUrl("file:///android_asset/mraid.js")
+
         mWebView.loadDataWithBaseURL("file:///android_asset/", getAdContent(), "text/html", "UTF-8", null)
         // 광고 컨텐츠 로드
 
@@ -29,31 +37,34 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        mWebView.addJavascriptInterface(object : Any() {
+        class WebAppInterface(private val mContext: Context) {
             @JavascriptInterface
-            fun postMessage(message: String?) {
-                Log.d("jhlee", "postMessage")
-                // 여기서 message는 MRAID에서 오는 메시지입니다.
-                // 예를 들어, MRAID 이벤트 처리를 여기서 할 수 있습니다.
+            fun open(url: String) {
+
+                Intent(Intent.ACTION_VIEW, Uri.parse(url)).run {
+                    this@MainActivity.startActivity(this)
+                }
             }
-        }, "Android")
+        }
+
+        mWebView.addJavascriptInterface(WebAppInterface(this), "Device")
 
         mWebView.setWebViewClient(object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                Log.d("jhlee", "shouldOverrideUrlLoading : " + request.url)
-                return super.shouldOverrideUrlLoading(view, request)
-            }
 
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
-                //                createMraidEnv();
-//                String test = "javascript:window.mraid.open(\"test~!\")";
-//                view.evaluateJavascript(test, null);
+                Log.d("jhlee", "onPageFinished")
+                val mraidEnvJson = createMraidEnv() // JSON 문자열을 생성하는 메소드 호출
+                val script = "javascript:(function() { window.MRAID_ENV = $mraidEnvJson; })();"
+                view.evaluateJavascript(script, null)
+                val test = "javascript:window.onMraidReady()"
+                view.evaluateJavascript(test, null)
+
             }
 
             override fun onLoadResource(view: WebView, url: String) {
                 super.onLoadResource(view, url)
-                Log.d("jhlee", "onLoadResource : $url")
+//                Log.d("jhlee", "onLoadResource : $url")
             }
         })
 
@@ -62,8 +73,105 @@ class MainActivity : AppCompatActivity() {
 
     private fun getAdContent(): String {
         // MRAID 광고 마크업 및 스크립트를 포함한 HTML 문자열 반환
-        return "<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "\t<head>\n" + "\t\t<meta charset=\"utf-8\" />\n" + "\t\t<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\" />\n" + "\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n" + "\n" + "\t\t<title></title>\n" + "\t\t<link rel=\"stylesheet\" href=\"css/main.css\" />\n" + "\t\t<link rel=\"icon\" href=\"images/favicon.png\" />\n" + "\n" + "\t\t<script src=\"mraid.js\"></script>\n" + "\t</head>\n" + "\n" + "\t<body>\n" + "\t\t<div id=\"imageContainer\">\n" + "\t\t\t<img src=\"https://nhnace.com/share/img/logo_main_ci.png\" width=\"300\" height=\"250\"/>\n" + "\t\t</div>\t\n" + "\t \n" + "    <script src=\"mraid.js\"></script>\n" +  // MRAID 스크립트 로드
-                "\t\t\n" + "\t\t<script>\n" + "\t\talert(mraid.getVersion());\n" + "\t\t\n" + "\t\tfunction addEvent(evnt, elem, func) {\n" + "\t\t\tif (elem.addEventListener) { // W3C DOM\n" + "\t\t\t\telem.addEventListener(evnt, func, false);\n" + "\t\t\t} else if (elem.attachEvent) { // IE DOM\n" + "\t\t\t\telem.attachEvent(\"on\" + evnt, func);\n" + "\t\t\t} else { // No much to do\n" + "\t\t\t\telem[evnt] = func;\n" + "\t\t\t}\n" + "\t\t}\n" + "\t\t\n" + "\t\tfunction displayAd()\n" + "\t\t{\n" + "\t\t\t//mraid.addEventListener(\"stateChange\", stateChangeHandler);\n" + "\t\t\t//mraid.addEventListener(\"sizeChange\", sizeChangeHandler);\n" + "\t\t\t//mraid.addEventListener(\"viewableChange\", viewableChangeHandler);\n" + "\t\t\t//mraid.addEventListener(\"error\", errorHandler);\n" + "\t\t\t\n" + "\t\t\tvar adContainer = document.querySelector('#imageContainer');\n" + "\t\t\taddEvent(\"click\", adContainer , function (e) {\n" + "\t\t\t\te.preventDefault();\n" + "\t\t\t\tmraid.open('https://www.nhnace.com/bannerinfo');\n" + "\t\t\t\t\n" + "\t\t\t\treturn false;\n" + "\t\t\t});\n" + "\t\t}\t\n" + "\t\t\n" + "\t\tfunction readyAdPage() {\n" + "\t\t\t mraid.removeEventListener(\"ready\", readyAdPage);\n" + "\n" + "\t\t\tdisplayAd();\n" + "\t\t}\n" + "\t\t\n" + "\t\tfunction detectAdPageStatus() {\n" + "\t\t\tvar success = false;\n" + "\n" + "\t\t\tif (document.readyState === 'complete') {\n" + "\t\t\t\n" + "\t\t\t\tif (typeof (mraid) === \"undefined\") {\n" + "\t\t\t\t\tsetTimeout(detectAdPageStatus, 250);\n" + "\t\t\t\t} else {\n" + "\t\t\t\t\tif (mraid.getState() === 'loading') {\n" + "\t\t\t\t\t\tmraid.addEventListener('ready', readyAdPage);\n" + "\t\t\t\t\t\t\n" + "\t\t\t\t\t\tmraid.addEventListener(error,function(message,action){\n" + "\t\t\t\t\t\t\talert('Error [' + action + ']' + message);\n" + "\t\t\t\t\t\t})\n" + "\t\t\t\t\t} else if (mraid.getState() === 'default') {\n" + "\t\t\t\t\t\tdisplayAd();\n" + "\t\t\t\t\t}\n" + "\t\t\t\t\t\n" + "\t\t\t\t\tsuccess = true;\n" + "\t\t\t\t}\n" + "\t\t\t}\n" + "\t\t\t\n" + "\t\t\treturn success;\t\n" + "\t\t}\n" + "\t\t\n" + "\t\tdetectAdPageStatus();\n" + "\t\t</script>\n" + "\t</body>\n" + "</html>"
+        val html = assets.open("mraid.html").bufferedReader().use { it.readText() }
+        return html
+
+//        return "<!DOCTYPE html>\n" +
+//                "<html lang=\"en\">\n" +
+//                "\t<head>\n" +
+//                "\t\t<meta charset=\"utf-8\" />\n" +
+//                "\t\t<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\" />\n" +
+//                "\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n" +
+//                "\n" +
+//                "\t\t<title></title>\n" +
+//                "\t\t<link rel=\"stylesheet\" href=\"css/main.css\" />\n" +
+//                "\t\t<link rel=\"icon\" href=\"images/favicon.png\" />\n" +
+//                "\n" +
+//                "\t\t<script src=\"mraid.js\"></script>\n" +
+//                "\t</head>\n" +
+//                "\n" +
+//                "\t<body>\n" +
+//                "\t\t<div id=\"imageContainer\">\n" +
+//                "\t\t\t<img src=\"https://nhnace.com/share/img/logo_main_ci.png\" width=\"300\" height=\"250\"/>\n" +
+//                "\t\t</div>\t\n" +
+//                "\t \n" +
+//                "\t\t<script src=\"js/scripts.js\"></script>\n" +
+//                "\t\t\n" +
+//                "\t\t<script>\n" +
+//                "\n" +
+//                "\t\tfunction onMraidReady() {\n" +
+//                "\t\t\tconsole.log(\"onMraidReady\")\n" +
+//                "\t\t\tmraid.addEventListener(\"stateChange\", stateChangeHandler);\n" +
+//                "\t\t\tdisplayAd();\n" +
+//                "\t\t}\n" +
+//                "\n" +
+//                "\t\tfunction stateChangeHandler() {\n" +
+//                "\t\t\tconsole.log(\"stateChangeHandler\")\n" +
+//                "\t\t}\n" +
+//                "\t\t\n" +
+//                "\t\tfunction addEvent(evnt, elem, func) {\n" +
+//                "\t\t\tif (elem.addEventListener) { // W3C DOM\n" +
+//                "\t\t\t\telem.addEventListener(evnt, func, false);\n" +
+//                "\t\t\t} else if (elem.attachEvent) { // IE DOM\n" +
+//                "\t\t\t\telem.attachEvent(\"on\" + evnt, func);\n" +
+//                "\t\t\t} else { // No much to do\n" +
+//                "\t\t\t\telem[evnt] = func;\n" +
+//                "\t\t\t}\n" +
+//                "\t\t}\n" +
+//                "\t\t\n" +
+//                "\t\tfunction displayAd()\n" +
+//                "\t\t{\n" +
+//                "\t\t\t//mraid.addEventListener(\"stateChange\", stateChangeHandler);\n" +
+//                "\t\t\t//mraid.addEventListener(\"sizeChange\", sizeChangeHandler);\n" +
+//                "\t\t\t//mraid.addEventListener(\"viewableChange\", viewableChangeHandler);\n" +
+//                "\t\t\t//mraid.addEventListener(\"error\", errorHandler);\n" +
+//                "\t\t\tconsole.log(\"displayAd\");\n" +
+//                "\t\t\tvar adContainer = document.querySelector('#imageContainer');\n" +
+//                "\t\t\taddEvent(\"click\", adContainer , function (e) {\n" +
+//                "\t\t\t\tconsole.log(\"click\");\n" +
+//                "\t\t\t\te.preventDefault();\n" +
+//                "\t\t\t\tmraid.open('https://www.nhnace.com/bannerinfo');\n" +
+//                "\t\t\t\t\n" +
+//                "\t\t\t\treturn false;\n" +
+//                "\t\t\t});\n" +
+//                "\t\t}\t\n" +
+//                "\t\t\n" +
+//                "\t\tfunction readyAdPage() {\n" +
+//                "\t\t\t mraid.removeEventListener(\"ready\", readyAdPage);\n" +
+//                "\n" +
+//                "\t\t\tdisplayAd();\n" +
+//                "\t\t}\n" +
+//                "\t\t\n" +
+//                "\t\tfunction detectAdPageStatus() {\n" +
+//                "\t\t\tconsole.log(\"detectAdPageStatus\");\n" +
+//                "\t\t\tvar success = false;\n" +
+//                "\n" +
+//                "\t\t\tif (document.readyState === 'complete') {\n" +
+//                "\t\t\t\n" +
+//                "\t\t\t\tif (typeof (mraid) === \"undefined\") {\n" +
+//                "\t\t\t\t\tsetTimeout(detectAdPageStatus, 250);\n" +
+//                "\t\t\t\t} else {\n" +
+//                "\t\t\t\t\tif (mraid.getState() === 'loading') {\n" +
+//                "\t\t\t\t\t\tmraid.addEventListener('ready', readyAdPage);\n" +
+//                "\t\t\t\t\t\t\n" +
+//                "\t\t\t\t\t\tmraid.addEventListener(error,function(message,action){\n" +
+//                "\t\t\t\t\t\t\talert('Error [' + action + ']' + message);\n" +
+//                "\t\t\t\t\t\t})\n" +
+//                "\t\t\t\t\t} else if (mraid.getState() === 'default') {\n" +
+//                "\t\t\t\t\t\tdisplayAd();\n" +
+//                "\t\t\t\t\t}\n" +
+//                "\t\t\t\t\t\n" +
+//                "\t\t\t\t\tsuccess = true;\n" +
+//                "\t\t\t\t}\n" +
+//                "\t\t\t}\n" +
+//                "\t\t\t\n" +
+//                "\t\t\treturn success;\t\n" +
+//                "\t\t}\n" +
+//                "\t\t\n" +
+//                "\t\t// detectAdPageStatus();\n" +
+//                "\t\t</script>\n" +
+//                "\t</body>\n" +
+//                "</html>";
 
 //        return "<!DOCTYPE html>\n" +
 //                "<html>\n" +
@@ -81,5 +189,20 @@ class MainActivity : AppCompatActivity() {
 //                "    </script>\n" +
 //                "</body>\n" +
 //                "</html>\n";
+    }
+
+    private fun createMraidEnv(): String {
+        val obj = JSONObject()
+        try {
+            obj.put("version", "1.0.3")
+            obj.put("sdk", "NHNACE_ADLIB")
+            obj.put("sdkVersion", "1.0.3")
+            obj.put("appId", packageName)
+            obj.put("adid", "1111")
+            obj.put("limitedAdTracking", true)
+        } catch (e: JSONException) {
+            throw RuntimeException(e)
+        }
+        return obj.toString()
     }
 }
